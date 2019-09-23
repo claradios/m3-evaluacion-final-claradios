7d94d19339c976daa2dd3870da77c1a0718c97e8
claradios <clara.dios.diez@gmail.com>
2019-09-23 08:46:00 +0200

    painting name species an img as required on exercise

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import './App.css';
-import {fetchCharacters} from './services/fetchCharacters';
+import { fetchCharacters } from './services/fetchCharacters';
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      api : []
+      api: []
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getCharacters();
   }
-  getCharacters(){
+  getCharacters() {
     fetchCharacters()
-    .then(data=>{
-      this.setState({
-        api:data.results
+      .then(data => {
+        this.setState({
+          api: data.results
+        })
       })
-    })
   }
 
   render() {
     return (
       <div className="App">
         <ul>
-          {this.state.api.map(item=>
-            <li>{item.name}</li>
+          {this.state.api.map(item =>
+            <li>
+              <div>
+                <h2>{item.name}</h2>
+                <p>{item.species}</p>
+                <div><img src={item.image} alt={`portrait of ${item.name}`}></img></div>
+              </div>
+            </li>
           )}
         </ul>
       </div>
