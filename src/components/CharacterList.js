79d9e2c3d9a95dbb3cb1dd0821e46e45d2213e45
claradios <clara.dios.diez@gmail.com>
2019-09-23 20:09:24 +0200

    finish refactoring ready to merge

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -19,7 +19,6 @@ const CharacterList = props => {
                                 image={item.image}
                             ></CharacterCard>
                         </Link>
-
                     </li>
                 )}
         </ul>);
