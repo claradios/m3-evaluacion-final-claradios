fcdd0096ce640d4a146cdee4fdd2faa38ebc026d
claradios <clara.dios.diez@gmail.com>
2019-09-23 20:38:15 +0200

    create folders to publish

---
+++ docs/precache-manifest.9c1d3a22c3fa2af8be909a4aaed8fb66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4035cfab1f7918b2ff2335b7c9cc4e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53d1a149f52b014fa3c9",
+    "url": "./static/css/main.74e1c4a2.chunk.css"
+  },
+  {
+    "revision": "0e54ecb383fa5c9d8d85",
+    "url": "./static/js/2.8470d003.chunk.js"
+  },
+  {
+    "revision": "53d1a149f52b014fa3c9",
+    "url": "./static/js/main.6cc5d794.chunk.js"
+  },
+  {
+    "revision": "ced67f6ab4885013a86e",
+    "url": "./static/js/runtime-main.32f9f168.js"
+  }
+]);
